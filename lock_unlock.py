878ae0adbde57464cee0c800fcdb833e79e98f3b
iggprt <33717256+iggprt@users.noreply.github.com>
2018-05-02 17:33:36 +0300

    update

--- lock_unlock.py
+++ lock_unlock.py
@@ -1,92 +1,48 @@
 import ctypes, time, threading
 import msvcrt
 import sys
+import stamp
 
 user32 = ctypes.windll.User32
 OpenDesktop = user32.OpenDesktopA
 SwitchDesktop = user32.SwitchDesktop
 DESKTOP_SWITCHDESKTOP = 0x0100
 
-start_time = 0
-due_time = 0
 prev_state = -1
-lock = 0
-unlock = 0
 flg = 0
-time_left = 0
-worked = 0
-breaked = 0
-last_lock = 0
-last_unlock = 0
 state = 1
-lock = threading.Lock()
-
-#its a function to write in this file
-def print_in_file(msg):
-	file = open("work.log","a+")
-	file.write(msg)
-	file.close()
 
 # its a flag to exit all threads
 def exit_flag():
 	if flg: 
 		return 1
 	return 0
-
-# display the time nicely
-def	time_str(a):
-	hours = int(a/3600)
-	msg = str(hours)
-	msg += ':'
-	minutes = int((a-hours*3600)/60)
-	msg += str(int((a-hours*3600)/60))
-	secs = int(a-(hours*3600) - minutes*60)
-	msg+= ":"+str(secs)
-	return msg
-
-def cyclic():
-
-	global prev_state
-	global time_left, last_lock, last_unlock
-	global worked, breaked
-	global state
 	
-	time_left = due_time - worked - start_time
+def log_write_cycle():
+	global state
+	stamp.stamp(state)
+	print state
+	threading.Timer(2, log_write_cycle).start()
+
+	
+def cyclic():
+	global prev_state,state
 	
 	hDesktop = OpenDesktop ("default", 0, False, DESKTOP_SWITCHDESKTOP)
 	#state = SwitchDesktop (hDesktop)	
 	
-	#detect change of state
-	if prev_state !=  state:
-		if state == 1:
-			if last_lock != 0:
-				breaked += time.time() - last_lock		
-			last_unlock = time.time()
-			print "unlocked: " + time_str(breaked) + " " + time.ctime()
-		else:
-			print "locked:   " + time_str(worked)  + " " + time.ctime()
-			worked += time.time() - last_unlock
-			last_lock = time.time()
-			print_in_file( "\nwork sesion started at: " + time.ctime(last_unlock) + " and endded at: " + time.ctime(last_lock) + " been working: " + time_str(time.time() - last_unlock))
+	stamp.stamp(state)
 
 	if exit_flag():
-		worked += time.time() - last_unlock
-		last_lock = time.time()
-		
-		msg = "\nwork sesion started at: " + time.ctime(last_unlock) + " and endded at: " + time.ctime(last_lock) + " been working: " + time_str(time.time() - last_unlock)
-		msg += "\nday endded at: " + time.ctime()
-		msg += "\nworked this day: " + time_str(worked)
-		msg += "\nbreaked this day: " + time_str(breaked)
-		
-		print_in_file(msg)
+		stamp.stamp(0)
+		stamp.write_report()
 		sys.exit()
 		
 	prev_state = state
 	threading.Timer(1, cyclic).start()
 
 def looking_for_key():
-	global flg
-	global due_time, last_lock, last_unlock, breaked, worked, state, start_time
+	global flg, state
 	while True:
 		if msvcrt.kbhit():
 			key = msvcrt.getch()
@@ -95,10 +51,7 @@ def looking_for_key():
 				flg = 1
 				sys.exit()
 			if key == 'a':
-				print "\n" + time.ctime(time.time() + (due_time - start_time - (time.time()-last_unlock) - worked) + 2700)
-				print time_str(due_time - start_time - (time.time()-last_unlock) - worked) + " l"
-				print time_str(time.time()-last_unlock + worked) + " w"
-				print time_str(breaked) + " b"
+				print "hm..."
 			if key == 'l':
 				state = 0
 			if key == 'u':
@@ -106,16 +59,15 @@ def looking_for_key():
 
 	threading.Timer(1, looking_for_key).start()
 	
+
 def Main():
-	global start_time, due_time, last_unlock
-	print("press: a for info or q to quit")
-	print_in_file("\n\nDay started at: " + time.ctime())
-	start_time = time.time()
-	due_time = start_time + 31500;
-	last_unlock = time.time()
+	
+	stamp.write_report()
+	stamp.stamp(1)	
 	
 	cyclic()
 	looking_for_key()
+	#log_write_cycle()	
 	
 if __name__ == "__main__":
 	Main()
--- stamp.py
+++ stamp.py
@@ -1,62 +1,77 @@
 import time, ctypes
 import csv
-import threading
-import random
+import os
+import lock_unlock
 
-def stamp( state ,time_):
+def stamp( state ):
 	with open("wl.log","a") as log_file:
 		f_writer = csv.writer(log_file, delimiter = ',')
-		#f_writer.writerow([str(state), str(int(time.time()))])
-		f_writer.writerow([str(state), time_])
-		print ([str(state), str(int(time.time()))])
+		f_writer.writerow([str(state), str(int(time.time()))])
+		print lock_unlock.state
 
-def write_raport():
-	rap_file = open("raport.csv","a")
+def write_report():
+
+	rap_file = open("raport.csv","ab")
 	fw = csv.writer(rap_file, delimiter = ',')
 
-	log_file = open("wl.log", "r")
-	fr = list(csv.reader(log_file))
+	try:
+		log_file = open("wl.log", "r")
+		fr = list(csv.reader(log_file))
+	except IOError:
+		return 0
 
 	prev_state = fr[0][0]
-	last_stamp = fr[0][1]
-
-	index = 0
+	last_stamp = int(fr[0][1])
+	
 	for row in fr:
-		index +=1
 		if row[0] != prev_state:
-			fw.writerow([str(index), prev_state, time.ctime(int(last_stamp)), 
-				time.ctime(int(row[1])), str(int(row[1]) - int(last_stamp))])
-			last_stamp = row[1]
+			w_row = []
+			
+			#time stamp	
+			w_row.append(str(row[1]))
+			
+			#date
+			date = time.strftime("%m/%d/%Y", time.gmtime(int(row[1])))
+			w_row.append(date)
+			
+			#state
+			w_row.append(prev_state)
+			
+			#start time
+			w_row.append(time.strftime("%H:%M:%S", time.gmtime(last_stamp)))
+			
+			#end time
+			w_row.append(time.strftime("%H:%M:%S", time.gmtime(int(row[1]))))
+			
+			#interval 
+			session_len = int(row[1]) - last_stamp
+			w_row.append(time.strftime("%H:%M:%S", time.gmtime(session_len)))
+			
+			print w_row
+			
+			if session_len <= 8*3600:
+				fw.writerow(w_row)
+			else:
+				fw.writerow([])
+			last_stamp = int(row[1])
 			prev_state = row[0]
 	rap_file.close()
 	log_file.close()
+	
+	os.remove("wl.log")
+	
+	return 0
 
-prev_rand = 1
-def debug_wirte_log():
-	global prev_rand
-	time_ = int(time.time())
-
-	for _ in range(500):
-		rand = random.random()
-		if prev_rand == 1:
-			rand += 0.45
-		else:
-			rand -=0.45
-
-		if (rand > 0.5):
-			stamp(1,time_)
-			time_ +=3
-			prev_rand = 1
-		else:
-			stamp(0, time_)
-			time_ += 1
-			prev_rand = 0
-
-start = time.time()
-
-#debug_wirte_log()
-write_raport()
-
-print (time.time()-start)
-
-
+def start_stamp():
+	log_file = open("wl.log", "r")
+	fr = csv.reader(log_file)
+	start = next(fr)[0]
+	log_file.close()	
+	return start
+	
+def Main():
+	write_report()
+	
+if __name__ == "__main__":
+	Main()
+	
